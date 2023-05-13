955dbe52df815fec863c307b6574e18e5f02d450
Veppam2 <v23ec02@ciencias.unam.mx>
2023-05-12 18:06:34 -0600

    Por terminar el inicio de sesión para la mesa

--- djangoProject/proyecto/cincuentaAmigos/forms.py
+++ djangoProject/proyecto/cincuentaAmigos/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Mesa
+
+tus
 
 class AgregarNombre(forms.Form):
     nombre = forms.CharField(label = 'ingresa tu nombre', max_length =10)
@@ -9,3 +12,14 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model= User
         fields =['username', "first_name", "last_name","email", "password1", "password2"]
+
+class MesaForm(forms.Form):
+    numero_mesa = forms.IntegerField(
+        label = "Número de Mesa",
+        required = True,
+    )
+    ubicacion = forms.CharField(
+        label = "Ubicación",
+        required = True,
+        max_length = 100
+    )
