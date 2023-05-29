ba38b1c12d44ec1d278a1b8c104c2605e6f17ce4
Veppam2 <v23ec02@ciencias.unam.mx>
2023-05-29 17:06:34 -0600

    FIX: Vistas únicas del administrador

--- djangoProject/proyecto/cincuentaAmigos/admin.py
+++ djangoProject/proyecto/cincuentaAmigos/admin.py
@@ -3,13 +3,7 @@ from django.contrib import admin
 from .models import Categoria
 from .models import Helado
 from .models import Alimento
-from .models import Mesa
-from .models import Carrito
-from .models import Votacion
 
 admin.site.register(Categoria)
 admin.site.register(Helado)
 admin.site.register(Alimento)
-admin.site.register(Mesa)
-admin.site.register(Carrito)
-admin.site.register(Votacion)
